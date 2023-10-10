41521c57da0e511f366291a38b6150976d3bd411
0sugo <josugo26@gmail.com>
2023-10-10 17:32:49 +0300

    Add diffferent sections to app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import Nav from './components/Nav/Nav'
+import Nav from './components/Nav/Nav';
+import Top from "./components/Top";
+import Hero from "./components/Hero";
+
 function App() {
   return (
     <div>
+      <Top />
       <Nav/>
+      <Hero />
     </div>
   );
 }
