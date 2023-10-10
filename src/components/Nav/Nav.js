1fb3994ad2b7a7f734722d92f53d83f99dc2d234
0sugo <josugo26@gmail.com>
2023-10-10 17:33:48 +0300

    Hide logo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from './logo.jpeg'
+// import logo from './logo.jpeg';
 const Nav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
