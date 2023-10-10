a766b0204b0d3d394a1e013f48a420498d059af0
0sugo <josugo26@gmail.com>
2023-10-10 17:31:11 +0300

    Add hero image

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import heropic from './images/heropic.jpg';
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <img src={heropic} alt='hero pic' className='w-screen'/>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
