c3e2ddb31616d96978063889792641ee7b71fe02
0sugo <josugo26@gmail.com>
2023-10-12 23:54:12 +0300

    Add classnames

--- src/components/About.js
+++ src/components/About.js
@@ -36,24 +36,24 @@ const About = () => {
       <hr className='w-full my-2 text-black'/>
 
       <div className="w-1/2 mx-auto mt-8">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+      <form className="">
+        <div className="">
+          <label className="" htmlFor="username">
             Username
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className=""
             id="username"
             type="text"
             placeholder="Username"
           />
         </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+        <div className="">
+          <label className="" htmlFor="password">
             Password
           </label>
           <input
-            className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            className=""
             id="password"
             type="password"
             placeholder="******************"
