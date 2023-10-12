cf6c535ffac901b62836cb6be117c14a8e0560c0
0sugo <josugo26@gmail.com>
2023-10-12 22:28:29 +0300

    Update About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,28 @@
 import React from 'react'
-
+import guarantee from './images/guarantee.svg';
+import project_work from './images/project_work.svg';
+import syllabus from './images/syllabus.svg';
 const About = () => {
   return (
     <div>
       <h1>Study at Mwala</h1>
-      <p>The Mwala Technical and Vocational College(MTVC) offers quality
-education in various courses together with industrial attachment in
-all syllabus as required by the curriculum as an intergral part of
-training. </p>
+      <p>
+        The Mwala Technical and Vocational College(MTVC) offers quality
+        education in various courses together with industrial attachment in
+        all syllabus as required by the curriculum as an intergral part of
+        training.
+      </p>
+      <div>
+        <img src={guarantee} alt='guarantee' className='w-20'/>
+      </div>
+
+      <div>
+        <img src={project_work} alt='project_work' className='w-20'/>
+      </div>
+
+      <div>
+        <img src={syllabus} alt='syllabus' className='w-20'/>
+      </div>
     </div>
   )
 }
