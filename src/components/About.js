c95aa6a3aa8842a213085329ce3b26c01ad796e5
0sugo <josugo26@gmail.com>
2023-10-12 23:09:36 +0300

    Add horizontal lines

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react'
 import guarantee from './images/guarantee.svg';
 import project_work from './images/project_work.svg';
 import syllabus from './images/syllabus.svg';
+
 const About = () => {
   return (
     <div>
@@ -18,15 +19,22 @@ const About = () => {
         <p>Guarantee attachment and insuarance cover</p>
       </div>
 
+      <hr className='w-full my-2 text-black'/>
+
       <div className='flex items-center'>
         <img src={project_work} alt='project_work' className='w-20 mr-3'/>
         <p>Sufficient practical work</p>
       </div>
 
+      <hr className='w-full my-2 text-black'/>
+
       <div className='flex items-center'>
         <img src={syllabus} alt='syllabus' className='w-20 mr-3'/>
         <p>Full syllabus coverage</p>
       </div>
+
+      <hr className='w-full my-2 text-black'/>
+
     </div>
   )
 }
