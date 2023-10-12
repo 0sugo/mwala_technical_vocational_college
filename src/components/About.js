469d18b6378e6a7ac68c01d26f19c72f5a9f1893
0sugo <josugo26@gmail.com>
2023-10-12 21:43:23 +0300

    Add about paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,10 @@ const About = () => {
   return (
     <div>
       <h1>Study at Mwala</h1>
+      <p>The Mwala Technical and Vocational College(MTVC) offers quality
+education in various courses together with industrial attachment in
+all syllabus as required by the curriculum as an intergral part of
+training. </p>
     </div>
   )
 }
