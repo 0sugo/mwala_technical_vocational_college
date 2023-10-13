9bf59ebf88ca9829d9275f18b29f71b076f84368
0sugo <josugo26@gmail.com>
2023-10-13 10:32:54 +0300

    Add findcourse component in app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from './components/Nav/Nav';
 import Top from "./components/Top";
 import Hero from "./components/Hero";
+import FindCourse from "./components/FindCourse";
 import About from "./components/About";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Top />
       <Nav/>
       <Hero />
+      <FindCourse />
       <About />
     </div>
   );
