7c14e3768ab456e6a6755e172007b84e1964e539
0sugo <josugo26@gmail.com>
2023-10-14 19:32:09 +0300

    Add events component

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import adm from './images/adm3.png'
+import principal from './images/principal.jpeg'
 
 const Events = () => {
   return (
     <div>
       <h1>College events</h1>
+
+      <div>
+        <img src={adm} alt='Admission'/>
+        <div>
+          <h3>News on Admissions</h3>
+          <p>Admission is currently on going, all applicants are required to apply
+  before the dealine and have all the neccessities required before
+  admission.</p>
+  <button type='button'>Learn more</button>
+
+        </div>
+      </div>
+
+      <div>
+        <img src={principal} alt='Principal'/>
+        <div>
+          <h3>News from Principal</h3>
+          <p>Admission is currently on going, all applicants are required to apply
+  before the dealine and have all the neccessities required before
+  admission.</p>
+  <button type='button'>Learn more</button>
+
+        </div>
+      </div>
     </div>
   )
 }
