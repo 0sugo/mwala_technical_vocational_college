d0ab327228587e75faffbea0212a3a604f0b858e
0sugo <josugo26@gmail.com>
2023-10-14 19:31:53 +0300

    Add findcourse component

--- src/components/FindCourse.js
+++ src/components/FindCourse.js
@@ -84,7 +84,7 @@ const FindCourse = () => {
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
           >
-            Sign In
+            Enquire
           </button>
         </div>
       </form>
