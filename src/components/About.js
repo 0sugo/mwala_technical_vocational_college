4aa42c6c21b99d68d497837ba1e55b34f4cd7d5e
0sugo <josugo26@gmail.com>
2023-10-15 00:05:47 +0300

    Add styles for about

--- src/components/About.js
+++ src/components/About.js
@@ -5,25 +5,29 @@ import about2 from './images/about2.jpg'
 const About = () => {
   return (
     <div>
-      <h1>A bit about us</h1>
-      <div>
-        <img src={about1} alt='about pic' />
-      <p>We are middle college situated 28 kilometers East of
-Machakos Town . Our college is specializing in training
-courses in Technical and business. Our students are
-trained with mind to fulfill employability challenge of the
-21st century</p>
+      <h1 className='text-center my-4'>A bit about us</h1>
 
+      <div className='flex flex-col-reverse'>
+        <div className='flex justify-end'>
+          <img src={about1} alt='about pic' className='rounded-lg'/>
+        </div>
+
+        <p>We are middle college situated 28 kilometers East of
+          Machakos Town . Our college is specializing in training
+          courses in Technical and business. Our students are
+          trained with mind to fulfill employability challenge of the
+          21st century</p>
       </div>
 
-      <div>
-        <img src={about2} alt='about pic' />
-      <p>We also provide other social ammenities and student
-welfare through our dean of students department to help
-our student grow mentally, physically and emotionally.
-This keeps our student at the very best making them
-productive, efficient and well disciplined.</p>
-
+      <div className='flex  flex-col-reverse my-4'>
+        <div className='flex justify-start'>
+          <img src={about2} alt='about pic' className='rounded-lg' />
+        </div>
+        <p>We also provide other social ammenities and student
+            welfare through our dean of students department to help
+            our student grow mentally, physically and emotionally.
+            This keeps our student at the very best making them
+            productive, efficient and well disciplined.</p>
       </div>
 
     </div>
