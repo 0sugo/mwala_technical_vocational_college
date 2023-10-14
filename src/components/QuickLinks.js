a740dbca2e5fdd83246112a51f9e4fcd0e59206f
0sugo <josugo26@gmail.com>
2023-10-14 19:31:17 +0300

    Add quicklink component

---
+++ src/components/QuickLinks.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const QuickLinks = () => {
+  return (
+    <div>
+      <h1>QuickLinks</h1>
+      <div>
+        <a href='/'>Student portal</a>
+        <a href='/'>Student union</a>
+        <a href='/'>Student welfare</a>
+        <a href='/'>Alumni wall of fame</a>
+        <a href='/'>Rules and regulations</a>
+        <a href='/'>Online registration and timetable</a>
+      </div>
+
+      <div>
+        <a href='/'>legal notice</a>
+        <a href='/'>Staff portal</a>
+        <a href='/'>Staff Vacancy</a>
+        <a href='/'>Sport activities</a>
+        <a href='/'>Tender's application</a>
+        <a href='/'>Environment policy</a>
+      </div>
+
+      <div>
+        <a href='/'>Our intake</a>
+        <a href='/'>School timetable</a>
+        <a href='/'>Registration form</a>
+        <a href='/'>Examination rules</a>
+        <a href='/'>Admission requirement</a>
+        <a href='/'>Accessing exam cards</a>
+      </div>
+
+    </div>
+  )
+}
+
+export default QuickLinks
