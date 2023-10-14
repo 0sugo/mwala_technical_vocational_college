68e76c6fb36f8e4529217c51f597b0f75fd846ab
0sugo <josugo26@gmail.com>
2023-10-15 00:05:37 +0300

    Add styles for events

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,28 +5,35 @@ import principal from './images/principal.jpeg'
 const Events = () => {
   return (
     <div>
-      <h1>College events</h1>
-
-      <div>
+      <h1 className='text-center my-4'>College events</h1>
+      <div className='flex flex-col-reverse items-center'>
+        <div className='flex justify-center'>
         <img src={adm} alt='Admission'/>
-        <div>
+        </div>
+        <div className='flex flex-col'>
           <h3>News on Admissions</h3>
           <p>Admission is currently on going, all applicants are required to apply
-  before the dealine and have all the neccessities required before
-  admission.</p>
-  <button type='button'>Learn more</button>
-
+              before the dealine and have all the neccessities required before
+              admission.
+          </p>
+          <div className='flex justify-center my-4'>
+        <button type='button' className='bg-[#891110] p-2 rounded-lg'>Learn more</button>
+        </div>
         </div>
       </div>
 
-      <div>
+      <div className='flex flex-col-reverse items-center'>
+      <div className='flex justify-center'>
         <img src={principal} alt='Principal'/>
-        <div>
+      </div>
+        <div className='flex flex-col'>
           <h3>News from Principal</h3>
           <p>Admission is currently on going, all applicants are required to apply
   before the dealine and have all the neccessities required before
   admission.</p>
-  <button type='button'>Learn more</button>
+        <div className='flex justify-center my-4'>
+        <button type='button' className='bg-[#891110] p-2 rounded-lg'>Learn more</button>
+        </div>
 
         </div>
       </div>
