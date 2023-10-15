6bac0c2454d1b4b745dbad54ffdd3b2f8fd6887f
0sugo <josugo26@gmail.com>
2023-10-15 12:21:42 +0300

    Modify departments title

--- src/components/Departments.js
+++ src/components/Departments.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Departments = () => {
   return (
     <div>
-      <h1>Departments</h1>
+      <div className='flex flex-col mb-2'>
+  <h2 className='overflow-hidden mt-16 pb-2 relative'>
+  Departments
+    <span className="absolute bottom-0 left-0 w-1/5 h-1  bg-black"></span>
+  </h2>
+</div>
     </div>
   )
 }
