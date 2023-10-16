f0cf7a4b4e8ca867fc5b4df89e2794fe1e12a45e
0sugo <josugo26@gmail.com>
2023-10-16 23:56:26 +0300

    Update about

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
     <div className='mx-4'>
 
       <div className='flex flex-col mb-2'>
-  <h2 className='overflow-hidden mt-16 pb-2 relative'>
+  <h2 className='overflow-hidden mt-16 pb-2 relative uppercase text-2xl font-bold'>
   About us
     <span className="absolute bottom-0 left-0 w-1/5 h-1  bg-black"></span>
   </h2>
