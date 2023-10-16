f1b129691ee573f6f5f982b537d8eb8087d427de
0sugo <josugo26@gmail.com>
2023-10-16 23:56:36 +0300

    Update department

--- src/components/Departments.js
+++ src/components/Departments.js
@@ -4,7 +4,7 @@ const Departments = () => {
   return (
     <div className='mx-4'>
       <div className='flex flex-col mb-2'>
-  <h2 className='overflow-hidden mt-16 pb-2 relative'>
+  <h2 className='overflow-hidden mt-16 pb-2 relative uppercase text-2xl'>
   Departments
     <span className="absolute bottom-0 left-0 w-1/5 h-1  bg-black"></span>
   </h2>
