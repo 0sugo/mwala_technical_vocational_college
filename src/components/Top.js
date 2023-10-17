f62cf1833675c045dc4a70e4ca2a65b65fdb1d2c
0sugo <josugo26@gmail.com>
2023-10-17 14:00:55 +0300

    Update Top.js

--- src/components/Top.js
+++ src/components/Top.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Top = () => {
   return (
-    <div className='bg-[#F6F7FA] p-4'>
+    <div className='bg-[#06294D] w-full p-4'>
       <div >
         <ul className='flex flex-col justify-center'>
           <li className='flex justify-center items-center mr-3'>
-            <ion-icon name="call"></ion-icon>
-            <span className='ml-2'>0796536524 / 0722501259</span>
+            <ion-icon name="call" className='' id='whiter'></ion-icon>
+            <span className='ml-2 text-white' >0796536524 / 0722501259</span>
           </li>
 
           <li className='flex justify-center items-center '>
-            <ion-icon name="mail"></ion-icon>
-            <span className='ml-2'>mwalatvc@gmail.com</span>
+            <ion-icon name="mail" className='text-white'  id='whiter'></ion-icon>
+            <span className='ml-2 text-white'>mwalatvc@gmail.com</span>
           </li>
         </ul>
       </div>
