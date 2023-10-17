246880ad1d026d760306105600a57d8b8a5c9f9a
0sugo <josugo26@gmail.com>
2023-10-17 13:59:11 +0300

    Update about.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div className='mx-4'>
 
-      <div className='flex flex-col mb-2'>
+      <div className='flex flex-col mb-4'>
   <h2 className='overflow-hidden mt-16 pb-2 relative uppercase text-2xl font-bold'>
   About us
     <span className="absolute bottom-0 left-0 w-1/5 h-1  bg-black"></span>
@@ -18,7 +18,7 @@ const About = () => {
           <img src={about1} alt='about pic' className='rounded-lg'/>
         </div>
 
-        <p>We are middle college situated 28 kilometers East of
+        <p className='text-[#5a5a5a]'>We are middle college situated 28 kilometers East of
           Machakos Town . Our college is specializing in training
           courses in Technical and business. Our students are
           trained with mind to fulfill employability challenge of the
@@ -29,7 +29,7 @@ const About = () => {
         <div className='flex justify-start'>
           <img src={about2} alt='about pic' className='rounded-lg' />
         </div>
-        <p>We also provide other social ammenities and student
+        <p className='text-[#5a5a5a]'>We also provide other social ammenities and student
             welfare through our dean of students department to help
             our student grow mentally, physically and emotionally.
             This keeps our student at the very best making them
