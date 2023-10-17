260decc5169468ce83c140a49bea2c980561bb07
0sugo <josugo26@gmail.com>
2023-10-17 14:01:09 +0300

    Update hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 import heropic from './images/heropic.jpg';
 const Hero = () => {
   return (
-    <div className='mx-3'>
+    <div className=''>
       <div>
         <img src={heropic} alt='hero pic' className='w-screen'/>
       </div>
