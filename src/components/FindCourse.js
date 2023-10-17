663c796e03a903b75e71c3863c44aa4a3f0e8e7a
0sugo <josugo26@gmail.com>
2023-10-17 23:26:30 +0300

    Update  findcourse.js

--- src/components/FindCourse.js
+++ src/components/FindCourse.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import guarantee from './images/guarantee.svg';
 import project_work from './images/project_work.svg';
 import syllabus from './images/syllabus.svg';
 
 const FindCourse = () => {
+  const [level ,setLevel] = useState('');
+
+  const handleLevel=(e)=>{
+    setLevel('diploma');
+  }
+  const diploma = ['Diploma in Electrical & Electronics Engineering (Power) Mod. 1, 2, 3',
+    'Diploma in Civil Engineering - Module 1, 2 and 3',
+    'Diploma in Building Technology – Module 1, 2 and 3',
+    'Diploma in Automotive Engineering - Module 1, 2 and 3',
+    'Diploma in Welding and Fabrication – Module 1, 2 and 3',
+    'Diploma in Fashion Design and Clothing - Module 1, 2 and 3',
+    'Diploma in Catering and Accommodation Mgt- Module 1, 2 and 3',
+    'Diploma in Food & Beverage– Module 1, 2 & 3',
+    'Diploma in Agriculture – Module 1, 2 and 3',
+    'Diploma in Analytical Chemistry – Module 1, 2 and 3',
+    'Diploma in Medical Engineering - Module 1, 2 and 3',
+    'Diploma in Mechanical Engineering – Module 1, 2 and 3',
+    'Diploma in Welding and Fabrication – Module 1, 2 and 3',
+    'Diploma in Petroleum and Geoscience – Module 1, 2 and 3.',
+    'Diploma in Information Comm. Tech - Module 1, 2 and 3',
+    'Diploma in Computer Science – Module 1, 2 and 3',
+    'Diploma in Social Work and Comm. Dev. – Module 1, 2 and 3',
+    'Diploma in Supply Chain Management – Module 1, 2 and 3',
+    'Diploma in Human Resource Management – Module 1, 2 and 3',
+    'Diploma in Project Management – Module 1, 2 and 3',
+    'Diploma in Secretarial Studies - Module 1, 2 and 3',
+    'Diploma in Child Care Protection Module 1, 2 and 3',
+    'Diploma in Tourism Management – Module 1, 2 and 3',
+    'Diploma in Banking and Finance – Module 1, 2 and 3',
+    'Diploma in Accounting and Finance – Module 1, 2 and 3'];
+    const certificate = [];
+
   return (
     <div className='mx-4'>
       <div className='flex flex-col mb-4'>
@@ -54,16 +86,18 @@ const FindCourse = () => {
             id="email"
             type="text"
             placeholder="Enter your email"
+            required
           />
         </div>
 
         <div className="mb-4">
-          <label className="block text-white-700 text-sm font-bold mb-2" htmlFor="studyLevel">
+          <label className="block text-white-700 text-sm  mb-2" htmlFor="studyLevel">
             Level of study
           </label>
           <select
             className="block w-full bg-white border border-gray-300 text-[#891110] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
             id="studyLevel"
+            onClick={handleLevel}
           >
             <option>Certificate</option>
             <option>Diploma</option>
@@ -71,18 +105,22 @@ const FindCourse = () => {
         </div>
 
         <div className="mb-4">
-          <label className="block text-sm font-bold mb-2" htmlFor="course">
+          <label className="block text-sm mb-2" htmlFor="course">
             Course
           </label>
           <select
-            className="block w-full bg-white border border-gray-300 text-[#891110] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
-            id="course"
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-          </select>
+  className="block w-full bg-white border border-gray-300 text-[#891110] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
+  id="course"
+>
+  {level === 'diploma' ? (
+    diploma.map((dip) => <option key={dip}>{dip}</option>)
+  ) : level === 'certificate' ? (
+    certificate.map((cert) => <option key={cert}>{cert}</option>)
+  ) : (
+    <option>Choose a level of study first . . .</option>
+  )}
+</select>
+
         </div>
 
         <div className="flex items-center justify-center">
