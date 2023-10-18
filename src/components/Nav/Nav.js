a4badfa6004857d3c7ca25abd1a8ebab3cfaf27a
0sugo <josugo26@gmail.com>
2023-10-18 17:02:07 +0300

    Replace logo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from './logo.jpeg';
+import logo from './logo.png';
 const Nav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
