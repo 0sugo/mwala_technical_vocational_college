26484ae18e15857f12a1752110f80697331233e6
0sugo <josugo26@gmail.com>
2023-11-06 23:18:24 +0300

    Modify new routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Top from "./components/Top";
 import { Routes, Route } from "react-router-dom";
 import AppliedSciences from "./components/AppliedSciences";
 import Outlet from "./components/Outlet";
+import IndustrialAttachment from "./components/IndustrialAttachment";
+import QualityAssuarance from "./components/QualityAssuarance";
+import FinanceDepartment from "./components/FinanceDepartment";
+import ExamDepartment from "./components/ExamDepartment";
+import PerformanceContracting from "./components/PerformanceContracting";
+import DeanStudents from "./components/DeanStudents";
+import DeputyPrincipal from "./components/DeputyPrincipal";
+import Principal from "./components/Principal";
 
 function App() {
   return (
@@ -14,6 +22,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Outlet />} />
         <Route path="/applied-sciences" element={<AppliedSciences />} />
+        <Route path="/Industrial-Attachment" element={<IndustrialAttachment />} />
+        <Route path="/Principal" element={<Principal />} />
+        <Route path="/Deputy-Principal" element={<DeputyPrincipal />} />
+        <Route path="/Dean-of-Students" element={<DeanStudents/>} />
+        <Route path="/Performance-Contracting" element={<PerformanceContracting/>} />
+        <Route path="/Exam-Department" element={<ExamDepartment/>} />
+        <Route path="/Finance-Department" element={<FinanceDepartment />} />
+        <Route path="/Quality-Assuarance" element={<QualityAssuarance/>} />
       </Routes>
     </div>
   );
