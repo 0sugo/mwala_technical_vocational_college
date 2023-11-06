92e4f368fd09cf04e28d872c11cc01a334a4bb47
0sugo <josugo26@gmail.com>
2023-11-06 22:01:31 +0300

    Update outet

---
+++ src/components/Outlet.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from "./Hero";
+import FindCourse from "./FindCourse";
+import About from "./About";
+import Events from "./Events";
+import Departments from "./Departments";
+import Footer from "./Footer";
+const Outlet = () => {
+  return (
+    <div>
+      <Hero />
+      <FindCourse />
+      <About />
+      <Events />
+      <Departments />
+      <Footer />
+    </div>
+  )
+}
+
+export default Outlet
