dafc3fd5b2483e6fd6a9242eccff05f07de85433
0sugo <josugo26@gmail.com>
2023-11-07 23:49:02 +0300

    Modify deputy Principal component

--- src/components/DeputyPrincipal.js
+++ src/components/DeputyPrincipal.js
@@ -2,7 +2,57 @@ import React from 'react'
 
 const DeputyPrincipal = () => {
   return (
-    <div>DeputyPrincipal</div>
+    <div>
+     <div className="mx-4">
+      <h2 className="text-cente">
+        DEPUTY PRINCIPAL
+      </h2>
+      <p className="pt-3">
+        The Office is responsible for assisting the principal in day-to-day management of the institution including the planning and overseeing the daily timetable of the classes.
+
+      </p>
+
+
+      <p className="pt-3">
+        {" "}
+        The office is also involved in other major activities which include:
+      </p>
+
+      <ul className="list-disc pl-10">
+        <li> Development of key institute’s strategic aims as well as participation in the formulation/review of academic policies and preparation /review of academic work manuals and procedures.</li>
+
+        <li>Corporate planning, staff recruitment, training of administrative staff, staff and students’ discipline, personnel administration and registry administration</li>
+
+        <li>Chairing a number of strategic institute committees, such as Institute Academic Board/ examinations and Timetable among others.</li>
+
+        <li>Working with the principal’s office in development of institutional and professional capacities aimed at enhancing the provision of quality and relevant technical education and training, including Staff Continuous Professional</li>
+
+        <li>The office is responsible for coordinating the development and implementation of the Institute Strategic Plan with regard to academic programs towards achievement of the intended short and long-term goals.</li>
+      </ul>
+
+      <h4 className="italic pt-3">2. Certificate Level</h4>
+      <ul className="list-disc pl-10">
+        <li>Craft certificate in General Agriculture, 2yrs.</li>
+        <li>Craft certificate in Laboratory Science Technology, 2yrs</li>
+      </ul>
+
+      <hr className="m-2" />
+    </div>
+
+    <div className="text-center bg-[#06294D] text-[white] p-4">
+        <div className="text-xs">
+          <span>Terms Of Use | </span>
+          <span>Legal Notice | </span>
+          <span>Website feedback</span>
+        </div>
+
+        <div className="text-xs">
+          <span>
+            2023 Mwala Technical & Vocation College. All Rights Reserved
+          </span>
+        </div>
+      </div>
+   </div>
   )
 }
 
