e0ed2f02cb306a06f731154a3093b62f8d9be32d
0sugo <josugo26@gmail.com>
2023-11-08 16:31:00 +0300

    Modify electricity department component

--- src/components/ElectricityDepartment.js
+++ src/components/ElectricityDepartment.js
@@ -2,7 +2,91 @@ import React from 'react'
 
 const ElectricityDepartment = () => {
   return (
-    <div>ElectricityDepartment</div>
+    <div>
+     <div className="mx-4">
+      <h2 className="text-cente">
+      ELECTRICAL AND ELECTRONIC ENGINEERING DEPARTMENT
+      </h2>
+
+      <h4 className="italic font-bold pt-3">Head of Department</h4>
+
+      <p className="pt-3">
+        Welcome to the Department of Electrical and Electronic Engineering at Mwala Technical and Vocational College! Our department is proud to be led by Mr. Hawkins Annan, a dynamic and passionate trainer who is committed to excellence in the field of Electrical and Electronic Engineering. With a passion for innovation, our staff brings a wealth of experience and knowledge to guide our students towards a bright future.
+
+      </p>
+
+      <h4 className="italic font-bold pt-3">Department Overview</h4>
+
+      <p className="pt-3">
+        Our department is divided into three sections, each specializing in a specific area of engineering:
+      </p>
+
+      <h4 className="italic pt-3">1. Electrical and Electronic Engineering Section</h4>
+      <ul className="list-disc pl-10">
+        <li>Diploma in Electrical and Electronic Engineering (Power Option).</li>
+        <li>Diploma in Electrical and Electronic Engineering (Telecommunication Option).</li>
+        <li>Craft Certificate in Electrical and Electronic Technology (Power Option).</li>
+        <li>Artisan Certificate in Electrical Installation.</li>
+        <li>NITA Grade III Electrical Wireman.</li>
+      </ul>
+
+      <h4 className="italic pt-3">2. Medical Engineering Section</h4>
+      <ul className="list-disc pl-10">
+        <li>Diploma in Medical Engineering.</li>
+      </ul>
+
+      <h4 className="italic pt-3">3. Medical Engineering Section</h4>
+      <ul className="list-disc pl-10">
+        <li>Diploma in Automotive Engineering.</li>
+        <li>Diploma in Mechanical Engineering (Plant Option).</li>
+        <li>Diploma in Mechanical Engineering (Production Option).</li>
+        <li>Diploma in Welding and Fabrication.</li>
+        <li>Craft Certificate in Automotive Engineering.</li>
+        <li>Artisan Certificate in Automotive Engineering</li>
+        <li>NITA Grade III Welding and Fabrication.</li>
+      </ul>
+
+
+
+      <h4 className="italic pt-3 font-bold">Electrical and Electronic Engineering Section:</h4>
+      <p>Our Electrical and Electronic Engineering Section offers a wide range of programs designed to equip students with the knowledge and skills necessary to excel in the rapidly evolving field of electrical and electronic technology. From power systems to telecommunications, our programs cover it all.</p>
+
+      <h4 className="italic pt-3 font-bold">Medical Engineering Section:</h4>
+      <p>In the Medical Engineering Section, we offer a comprehensive Diploma program in Medical Engineering. This program prepares students for careers in the healthcare industry, where they play a vital role in maintaining and servicing medical equipment, ensuring the safe and effective delivery of healthcare services</p>
+
+      <h4 className="italic pt-3 font-bold">Mechanical Engineering Section:</h4>
+      <p>Our Mechanical Engineering Section offers a diverse set of diploma courses including plant engineering, production engineering, welding, fabrication and automotive engineering. These programs provide students with hands-on training and theoretical knowledge, making them well-prepared for various roles in the mechanical engineering field.</p>
+
+      <h4 className="italic pt-3 font-bold">Mechanical Engineering Section:</h4>
+      <ul className="list-disc pl-10">
+        <li><span className='font-bold'>Qualified Faculty:</span>Our dedicated faculty members have extensive technical and academic expertise, ensuring that students receive the highest quality education.</li>
+        <li><span className='font-bold'>Industry Partnerships: </span>We collaborate with industry leaders to ensure that our programs are aligned with current industry trends and demands.</li>
+        <li><span className='font-bold'>Supportive Community:</span>At Mwala Technical and Vocational College we foster a supportive and inclusive community where students can thrive academically and personally.</li>
+
+        <p className='py-3'>Whether you aspire to become a skilled electrician, an electronics expert, a medical equipment technician, or a mechanical engineer, our department has the programs and resources to help you achieve your goals. We look forward to welcoming you to our department and helping you embark on a successful engineering career.</p>
+        <p>If you have any questions or would like to learn more about our programs, please do not hesitate to contact us. We are here to support your educational journey.</p>
+      </ul>
+
+      <h4 className="italic pt-4 font-bold text-xs">Hawkins Annan Balewa</h4>
+      <h4 className="italic font-bold text-xs">Head of Department</h4>
+
+      <hr className="m-2" />
+    </div>
+
+    <div className="text-center bg-[#06294D] text-[white] p-4">
+        <div className="text-xs">
+          <span>Terms Of Use | </span>
+          <span>Legal Notice | </span>
+          <span>Website feedback</span>
+        </div>
+
+        <div className="text-xs">
+          <span>
+            2023 Mwala Technical & Vocation College. All Rights Reserved
+          </span>
+        </div>
+      </div>
+   </div>
   )
 }
 
