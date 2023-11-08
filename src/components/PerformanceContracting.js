a0f7dbdd3c1083826e6e9648b162bdc1797d946d
0sugo <josugo26@gmail.com>
2023-11-08 21:10:32 +0300

    Add center feature

--- src/components/PerformanceContracting.js
+++ src/components/PerformanceContracting.js
@@ -4,7 +4,7 @@ const PerformanceContracting = () => {
   return (
     <div>
      <div className="mx-4">
-      <h2 className="text-cente">
+      <h2 className="text-center">
         PERFORMANCE CONTRACTING DEPARTMENT
       </h2>
       <p className="pt-3">
