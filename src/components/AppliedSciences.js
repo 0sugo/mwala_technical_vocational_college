797e37bd96240257dc34a751fa5fbd13ecbbc219
0sugo <josugo26@gmail.com>
2023-11-08 21:10:03 +0300

    Add center feature

--- src/components/AppliedSciences.js
+++ src/components/AppliedSciences.js
@@ -4,7 +4,7 @@ const AppliedSciences = () => {
   return (
    <div>
      <div className="mx-4">
-      <h2 className="text-cente">
+      <h2 className="text-center">
         AGRICULTURE AND APPLIED SCIENCE DEPARTMENT
       </h2>
       <p className="pt-3">
