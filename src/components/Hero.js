40ed8d1f316a6e4979122824ec7a4294a248005a
0sugo <josugo26@gmail.com>
2023-11-08 21:10:22 +0300

    Add center feature

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import heropic from './images/heropic.jpg';
+import Alpine from 'alpinejs';
+
 const Hero = () => {
 
-  function animation() {
-    return {
-      counter: 0,
-      animate(finalCount) {
-        let time = 1500
-        let interval = 9
-        let step = Math.floor(finalCount * interval / time)
-        let timer = setInterval(() => {
-          this.counter = this.counter + step;
-          if (this.counter >= finalCount + step) {
-            this.counter = finalCount
-            clearInterval(timer);
-            timer = null;
-            return;
-          }
-        }, interval);
-      }
-    };
-  }
+  const animation = () => ({
+    counter: 0,
+    animate(finalCount) {
+      const time = 1500;
+      const interval = 300;
+      const step = Math.floor(finalCount * interval / time);
+      const timer = setInterval(() => {
+        this.counter = this.counter + step;
+        if (this.counter >= finalCount + step) {
+          this.counter = finalCount;
+          clearInterval(timer);
+        }
+      }, interval);
+    },
+  });
 
+  useEffect(() => {
+    Alpine.data('animation', animation);
+    Alpine.start();
+  }, []);
 
   return (
     <div className=''>
@@ -29,37 +31,29 @@ const Hero = () => {
         <img src={heropic} alt='hero pic' className='w-screen' />
       </div>
 
-      <div class="antialiased h-28 px-4 bg-red-400 flex justify-evenly space-x-16 items-center text-center text-gray-800">
-        <div class="w-48 flex flex-col items-center">
-          <span class="text-4xl font-bold text-blue-900"
-            x-data="animation()"
-            x-init="animate(400)"
-            x-text="counter">
+
+      <div className="antialiased h-28 px-4  flex justify-evenly space-x-16 items-center text-center text-gray-800">
+        <div className="w-48 flex flex-col items-center">
+          <span className="text-4xl font-bold text-black " x-data="animation()" x-init="animate(50)" x-text="counter">
             0
           </span>
           <p className='font-bold'>Courses</p>
         </div>
-        <div class="w-48 flex flex-col items-center">
-          <span class="text-4xl font-bold text-blue-900"
-            x-data="animation()"
-            x-init="animate(9400)"
-            x-text="counter">
+        <div className="w-48 flex flex-col items-center">
+          <span className="text-4xl font-bold text-[#06294D]" x-data="animation()" x-init="animate(6)" x-text="counter">
             0
           </span>
           <p className='font-bold'>Years</p>
         </div>
-        <div class="w-48 flex flex-col items-center">
-          <span class="text-4xl font-bold text-blue-900"
-            x-data="animation()"
-            x-init="animate(11024)"
-            x-text="counter">
+        <div className="w-48 flex flex-col items-center">
+          <span className="text-4xl font-bold text-[#6B2D30]" x-data="animation()" x-init="animate(11024)" x-text="counter">
             0
           </span>
           <p className='font-bold'>Graduates</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
