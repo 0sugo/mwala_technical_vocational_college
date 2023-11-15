6f933afd4c3c82d002872f9533f4c342b6759853
0sugo <josugo26@gmail.com>
2023-11-15 10:02:33 +0300

    Modify findcourse

--- src/components/FindCourse.js
+++ src/components/FindCourse.js
@@ -4,9 +4,9 @@ import project_work from './images/project_work.svg';
 import syllabus from './images/syllabus.svg';
 
 const FindCourse = () => {
-  const [level ,setLevel] = useState('');
+  const [level, setLevel] = useState('');
 
-  const handLevelChange=(e)=>{
+  const handLevelChange = (e) => {
     setLevel(e.target.value);
   }
 
@@ -37,127 +37,127 @@ const FindCourse = () => {
     'Accounting Technician Diploma Level 2',
     'Accounting Technician Diploma Level',
     'Diploma in Accounting and Finance – Module 1, 2 and 3'];
-    const certificate = [
-      'Craft Certificate in Electrical Technology (Power) – Mod 1, 2',
-      'Craft Certificate in Building Technology – Module 1, 2',
-      'Certificate in Automotive Engineering- Module 1, 2',
-      'Craft certificates in welding and fabrication- Module 1, 2',
-      'Craft Certificate in Food & Beverage – Module 1, 2',
-      'Certificate in Fashion Design & Garment Making-Mod 1, 2',
-      'Certificate in Catering & Accommodation Operations- Module 1, 2',
-      'Craft Certificate in Science Laboratory Technology',
-      'Craft Certificate in Masonry',
-      'Craft Certificate in Plumbing',
-      'Craft Certificate in Agriculture',
-      'Certificate in Human Resource Management – Module 1, 2',
-      'Certificate in Supply Chain Management – Module 1, 2',
-      'Certificate in Information Studies - Module 1, 2',
-      'Certificate in Social Work Development. – Module 1, 2',
-      'Certificate in Project Management – Module 1, 2',
-      'Certificate in Secretarial Studies- Module 1, 2',
-      'Certificate in Child Care Protection Module 1, 2',
-      'Certificate in Accounting Management Skill (CAM) Level I',
-      'Certified Public Accounts- Foundation',
-      'Certified Public Accounts- Intermediate',
-      'Certified Public Accounts-Final'];
+  const certificate = [
+    'Craft Certificate in Electrical Technology (Power) – Mod 1, 2',
+    'Craft Certificate in Building Technology – Module 1, 2',
+    'Certificate in Automotive Engineering- Module 1, 2',
+    'Craft certificates in welding and fabrication- Module 1, 2',
+    'Craft Certificate in Food & Beverage – Module 1, 2',
+    'Certificate in Fashion Design & Garment Making-Mod 1, 2',
+    'Certificate in Catering & Accommodation Operations- Module 1, 2',
+    'Craft Certificate in Science Laboratory Technology',
+    'Craft Certificate in Masonry',
+    'Craft Certificate in Plumbing',
+    'Craft Certificate in Agriculture',
+    'Certificate in Human Resource Management – Module 1, 2',
+    'Certificate in Supply Chain Management – Module 1, 2',
+    'Certificate in Information Studies - Module 1, 2',
+    'Certificate in Social Work Development. – Module 1, 2',
+    'Certificate in Project Management – Module 1, 2',
+    'Certificate in Secretarial Studies- Module 1, 2',
+    'Certificate in Child Care Protection Module 1, 2',
+    'Certificate in Accounting Management Skill (CAM) Level I',
+    'Certified Public Accounts- Foundation',
+    'Certified Public Accounts- Intermediate',
+    'Certified Public Accounts-Final'];
 
   return (
     <div className='mx-4 md:mx-12'>
       <div className='flex flex-col mb-4'>
-  <p className=' overflow-hidden mt-16 pb-2 relative uppercase text-2xl font-bold'>
-    Study at Mwala
-    <span className="absolute bottom-0 left-0 w-1/5 h-1  bg-black"></span>
-  </p>
-</div>
+        <p className=' overflow-hidden mt-16 pb-2 relative uppercase text-2xl font-bold'>
+          Study at Mwala
+          <span className="absolute bottom-0 left-0 w-1/5 h-1  bg-black"></span>
+        </p>
+      </div>
 
-      <p className='greyer'>
+      <p className='greyer md:text-lg'>
         The Mwala Technical and Vocational College(MTVC) offers quality
         education in various courses together with industrial attachment in
         all syllabus as required by the curriculum as an intergral part of
         training.
       </p>
 
-      <div className='flex items-center mt-8'>
-        <img src={guarantee} alt='guarantee' className='w-16 mr-3'/>
+      <div className='flex items-center mt-8 md:text-lg'>
+        <img src={guarantee} alt='guarantee' className='w-16 mr-3' />
         <p>Guarantee attachment and insuarance cover</p>
       </div>
 
-      <hr className='w-full my-2 border-black'/>
+      <hr className='w-full my-2 border-black' />
 
-      <div className='flex items-center'>
-        <img src={project_work} alt='project_work' className='w-16 mr-3'/>
+      <div className='flex items-center md:text-lg'>
+        <img src={project_work} alt='project_work' className='w-16 mr-3' />
         <p>Sufficient practical work</p>
       </div>
 
-      <hr className='w-full my-2  border-black'/>
+      <hr className='w-full my-2  border-black' />
 
-      <div className='flex items-center'>
-        <img src={syllabus} alt='syllabus' className='w-16 mr-3'/>
+      <div className='flex items-center md:text-lg'>
+        <img src={syllabus} alt='syllabus' className='w-16 mr-3' />
         <p>Full syllabus coverage</p>
       </div>
 
-      <hr className='w-full my-2 border-black'/>
+      <hr className='w-full my-2 border-black' />
 
       <div className="bg-[#891110] p-4 mt-8 rounded-lg">
         <p className='text-center font-bold'>Find a course for you...</p>
-      <form className="" >
+        <form className="md:text-lg" >
 
-        <div className="mb-4">
-          <label className="text-slate-100" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="block appearance-none w-full placeholder-green-900 bg-white border border-gray-300 text-[#891110] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="text"
-            placeholder="Enter your email"
-            required
-          />
-        </div>
+          <div className="mb-4">
+            <label className="text-slate-100" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="block appearance-none w-full placeholder-green-900 bg-white border border-gray-300 text-[#891110] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="text"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
 
-        <div className="mb-4">
-          <label className="block text-white-700 text-sm  mb-2 text-slate-100" htmlFor="studyLevel">
-            Level of study
-          </label>
-          <select
-            className="block w-full bg-white border border-gray-300 text-[#891110] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
-            id="studyLevel"
-            onChange={handLevelChange}
-          >
-            <option disabled selected>Select level of study...</option>
-            <option>Certificate</option>
-            <option>Diploma</option>
-          </select>
-        </div>
+          <div className="mb-4">
+            <label className="block text-white-700 text-sm  mb-2 text-slate-100" htmlFor="studyLevel">
+              Level of study
+            </label>
+            <select
+              className="block w-full bg-white border border-gray-300 text-[#891110] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
+              id="studyLevel"
+              onChange={handLevelChange}
+            >
+              <option disabled selected>Select level of study...</option>
+              <option>Certificate</option>
+              <option>Diploma</option>
+            </select>
+          </div>
 
-        <div className="mb-4">
-          <label className="block text-sm mb-2 text-slate-100" htmlFor="course">
-            Course
-          </label>
-          <select
-  className="block w-full bg-white border border-gray-300 text-[#891110] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
-  id="course"
->
-  {level === 'Diploma' ? (
-    diploma.map((dip) => <option key={dip}>{dip}</option>)
-  ) : level === 'Certificate' ? (
-    certificate.map((cert) => <option key={cert}>{cert}</option>)
-  ) : (
-    <option className='text-[#cccccc]'>Choose a level of study first . . .</option>
-  )}
-</select>
+          <div className="mb-4">
+            <label className="block text-sm mb-2 text-slate-100" htmlFor="course">
+              Course
+            </label>
+            <select
+              className="block w-full bg-white border border-gray-300 text-[#891110] py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline"
+              id="course"
+            >
+              {level === 'Diploma' ? (
+                diploma.map((dip) => <option key={dip}>{dip}</option>)
+              ) : level === 'Certificate' ? (
+                certificate.map((cert) => <option key={cert}>{cert}</option>)
+              ) : (
+                <option className='text-[#cccccc]'>Choose a level of study first . . .</option>
+              )}
+            </select>
 
-        </div>
+          </div>
 
-        <div className="flex items-center justify-center">
-          <button
-            className="bg-[#06294D] hover:bg-[#06294D] text-slate-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-          >
-            Enquire
-          </button>
-        </div>
-      </form>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-[#06294D] hover:bg-[#06294D] text-slate-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Enquire
+            </button>
+          </div>
+        </form>
       </div>
 
     </div>
