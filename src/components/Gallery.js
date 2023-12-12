3081c0adf4a7ce8a93bfa3060cc63368d7bc13b0
0sugo <josugo26@gmail.com>
2023-12-12 17:48:06 +0300

    Update gallery section

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,41 +1,141 @@
 import React from 'react';
+import gallery1 from './images/gallery1.jpeg';
+import gallery2 from './images/gallery2.jpeg';
+import gallery3 from './images/gallery3.jpeg';
+import gallery4 from './images/gallery4.jpeg';
+import gallery5 from './images/gallery5.jpeg';
+import gallery6 from './images/gallery6.jpeg';
+import gallery7 from './images/gallery7.jpeg';
+import gallery8 from './images/gallery8.jpeg';
+import gallery9 from './images/gallery9.jpeg';
+import gallery10 from './images/gallery10.jpeg';
+import gallery11 from './images/gallery11.jpeg';
+import gallery12 from './images/gallery12.jpeg';
+import gallery13 from './images/gallery13.jpeg';
+import gallery14 from './images/gallery14.jpeg';
+import gallery15 from './images/gallery15.jpeg';
+import gallery16 from './images/gallery16.jpeg';
+import gallery17 from './images/gallery17.jpeg';
+import gallery18 from './images/gallery18.jpeg';
+import gallery19 from './images/gallery19.jpeg';
+import gallery20 from './images/gallery20.jpeg';
+import gallery21 from './images/gallery21.jpeg';
+import gallery22 from './images/gallery22.jpeg';
+import gallery23 from './images/gallery23.jpeg';
+import gallery24 from './images/gallery24.jpeg';
 
 const Gallery = () => (
-  <div>
-    <div className="container mx-auto p-4">
-      <h3 className="text-center my-4">Our Gallery</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover" data-mdb-ripple="true" data-mdb-ripple-color="dark">
-          <img width={500} height={300} src="https://i.postimg.cc/3JWTBStm/DSC-6179.jpg" className="w-full transition duration-300 ease-linear align-middle" alt="" />
-          <div>
-            <div className="hover-overlay">
-              <div className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"> </div>
-            </div>
+  <div class="max-w-screen-2xl mx-auto px-4 py-16 lg:py-24 relative bg-gray-50">
+    <div class="flex flex-col md:flex-row gap-2">
+      <div class="flex flex-1 flex-col gap-2">
+        <div class="flex flex-1 flex-col">
+          <img class="object-cover h-full" src={gallery1} alt="image1" />
+        </div>
+        <div class="md:flex flex-1 flex-row gap-2">
+          <div class="flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery2} alt="image1" />
+          </div>
+          <div class="md:flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery7} alt="image1" />
           </div>
         </div>
-        <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover" data-mdb-ripple="true" data-mdb-ripple-color="dark">
-          <img width={500} height={300} src="https://i.postimg.cc/3JWTBStm/DSC-6179.jpg" className="w-full transition duration-300 ease-linear align-middle" alt="" />
-          <div>
-            <div className="hover-overlay">
-              <div className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"> </div>
-            </div>
+      </div>
+      <div class="flex flex-1 flex-col gap-2">
+        <div class="md:flex flex-1 flex-row gap-2">
+          <div class="flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery4} alt="image1" />
+          </div>
+          <div class="md:flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery17} alt="image1" />
           </div>
         </div>
-        <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover" data-mdb-ripple="true" data-mdb-ripple-color="dark">
-          <img width={500} height={300} src="https://i.postimg.cc/3JWTBStm/DSC-6179.jpg" className="w-full transition duration-300 ease-linear align-middle" alt="" />
-          <div>
-            <div className="hover-overlay">
-              <div className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"> </div>
-            </div>
+        <div class="flex flex-1 flex-col">
+          <img class="object-cover h-full" src={gallery5} alt="image1" />
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col md:flex-row gap-2">
+      <div class="flex flex-1 flex-col gap-2">
+        <div class="flex flex-1 flex-col">
+          <img class="object-cover h-full" src={gallery21} alt="image1" />
+        </div>
+        <div class="md:flex flex-1 flex-row gap-2">
+          <div class="flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery8} alt="image1" />
+          </div>
+          <div class="md:flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery9} alt="image1" />
           </div>
         </div>
-        <div className="zoom shadow-lg rounded-lg relative overflow-hidden bg-no-repeat bg-cover" data-mdb-ripple="true" data-mdb-ripple-color="dark">
-          <img width={500} height={300} src="https://i.postimg.cc/3JWTBStm/DSC-6179.jpg" className="w-full transition duration-300 ease-linear align-middle" alt="" />
-          <div>
-            <div className="hover-overlay">
-              <div className="mask absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out"> </div>
-            </div>
+      </div>
+      <div class="flex flex-1 flex-col gap-2">
+        <div class="md:flex flex-1 flex-row gap-2">
+          <div class="flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery10} alt="image1" />
           </div>
+          <div class="md:flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery11} alt="image1" />
+          </div>
+        </div>
+        <div class="flex flex-1 flex-col">
+          <img class="object-cover h-full" src={gallery13} alt="image1" />
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col md:flex-row gap-2">
+      <div class="flex flex-1 flex-col gap-2">
+        <div class="flex flex-1 flex-col">
+          <img class="object-cover h-full" src={gallery15} alt="image1" />
+        </div>
+        <div class="md:flex flex-1 flex-row gap-2">
+          <div class="flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery14} alt="image1" />
+          </div>
+          <div class="md:flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery12} alt="image1" />
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-1 flex-col gap-2">
+        <div class="md:flex flex-1 flex-row gap-2">
+          <div class="flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery16} alt="image1" />
+          </div>
+          <div class="md:flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery6} alt="image1" />
+          </div>
+        </div>
+        <div class="flex flex-1 flex-col">
+          <img class="object-cover h-full" src={gallery18} alt="image1" />
+        </div>
+      </div>
+    </div>
+
+    <div class="flex flex-col md:flex-row gap-2">
+      <div class="flex flex-1 flex-col gap-2">
+        <div class="flex flex-1 flex-col">
+          <img class="object-cover h-full" src={gallery19} alt="image1" />
+        </div>
+        <div class="md:flex flex-1 flex-row gap-2">
+          <div class="flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery20} alt="image1" />
+          </div>
+          <div class="md:flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery3} alt="image1" />
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-1 flex-col gap-2">
+        <div class="md:flex flex-1 flex-row gap-2">
+          <div class="flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery22} alt="image1" />
+          </div>
+          <div class="md:flex flex-1 flex-col">
+            <img class="object-cover h-full" src={gallery23} alt="image1" />
+          </div>
+        </div>
+        <div class="flex flex-1 flex-col">
+          <img class="object-cover h-full" src={gallery24} alt="image1" />
         </div>
       </div>
     </div>
