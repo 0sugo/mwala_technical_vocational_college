e713316efef5d9b2fbe3e9d31f9ff431047efe77
0sugo <josugo26@gmail.com>
2024-02-23 18:27:52 +0300

    Update Principal name

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -96,8 +96,8 @@ const Principal = () => (
         Karanja Muia of North Carolina Central University.
       </p>
 
-      <h4 className="italic pt-4 font-bold text-xs">Martha Otwori</h4>
-      <h4 className="italic font-bold text-xs">Head of the Quality Assurance Office.</h4>
+      <h4 className="italic pt-4 font-bold text-xs">Kavita C. Kyuli </h4>
+      <h4 className="italic font-bold text-xs">Principal.</h4>
 
       <hr className="m-2" />
     </div>
